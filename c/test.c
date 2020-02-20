1b5c784e68a0df5522e038c8f093bfdd86ca30d7
QiGe <gridwang@gmail.com>
2020-02-20 23:08:50 +0800

    init

---
+++ c/test.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "string.h"
+void main(){
+    char correctPassword[] = "DaSiWoYeBuShuo";
+    char yourPassword[50];
+    printf("Please input your password:");
+    ...;
+    if(...){
+        printf("Yes, Please!");
+    }else{
+      printf("No, Get out!");
+    }
+}
